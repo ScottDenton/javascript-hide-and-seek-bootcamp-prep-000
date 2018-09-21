6f612ff545a3e6ea3cc34145562e366194ddcea0
scottyd_14 <scottyd_14@hotmail.com>
2018-09-21 21:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 function getFirstSelector(selector){
-  var firstSelector = document.querySelector(selector)
-  return firstSelector
+  //return document.querySelector(selector);
 }
-
-function nestedTarget(){
-  var nested = document.getElementById('nested')
-  var target = nested.querySelector('.target');
-  return target
-}
-
-function increaseRankBy(n) {
- var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
- for (var i = 0; i < ranks.length; i++){
-  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
- }
-  }
-  
- function deepestChild(){
-   
-   var grandNode = document.getElementById('grand-node').querySelectorAll('div');
-return grandNode[grandNode.length -1]
-
-    }
-    
     
     

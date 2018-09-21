4f25d4260fc1dadbdbdc2e3dc6d3c0244b3f7eee
scottyd_14 <scottyd_14@hotmail.com>
2018-09-21 21:58:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-function getFirstSelector(selector){
-  var firstSelector = document.querySelector(selector);
-  return firstSelector
+
+  function getFirstSelector(selector){
+    var firstSelector = document.querySelector(selector)
+    return firstSelector
 }
-    
-  
-  function nestedTarget(){
-    var nested = document.getElementById(nested);
-    var myTarget = nested.querySelector('.target');
-    return myTarget;
+
+function nestedTarget(){
+  var nested = document.getElementById('nested')
+  var target = nested.querySelector('.target');
+  return target
+}
+
+function increaseRankBy(n) {
+ var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ for (var i = 0; i < ranks.length; i++){
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+ }
   }
   
-  function increaseRankBy(n){
-  var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  
-  for (var i = 0; i < ranks.length; i ++){
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
-  }
-  }
-  
-  function deepestChild(){
-    
-    var grandNode = document.getElementById('grand-node').querySelectorAll('div');
-    
-    return grandNode.length-1
-  }
-  
-  
-  
-  
+ function deepestChild(){
+   
+   var grandNode = document.getElementById('grand-node').querySelectorAll('div');
+return grandNode[grandNode.length -1]
+
+    }
   
   
